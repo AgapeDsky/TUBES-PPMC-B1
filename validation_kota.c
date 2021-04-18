357d376e6a32072ff8b033e9f46fda09f47bd383
nugrahadiFR <60011325+nugrahadiFR@users.noreply.github.com>
2021-04-18 23:41:59 +0700

    Create validation_kota.c

---
+++ validation_kota.c
@@ -0,0 +1,14 @@
+#include "validation_kota.h"
+
+bool isalphabet(char c){
+    return ((c >= 'a' && c <= 'z') || (c <= 'A' && c >= 'Z') || (c == NULL));
+}
+
+bool isvalid(char* kota){
+    for(int i=0; strlen(kota); i++){
+        if(!isalphabet(kota[i])){
+            return 0;
+        }
+    }
+    return 1;
+}
