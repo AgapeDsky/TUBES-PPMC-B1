d5410293ca07dd3ad094860a0e1371a75b31a4c4
Agape D'sky <ikanblackghost@gmail.com>
2021-04-18 22:36:52 +0700

    TSP function v1

---
+++ search.c
@@ -0,0 +1,32 @@
+#include "search.h"
+#include "limits.h"
+#include <stdlib.h>
+
+double search (double** mat, int* arr, int n) {
+    if (n == 1) {
+        return 0;
+    }
+    else if (n == 2) {
+        return mat[arr[0]][arr[1]];
+    }
+    else{
+        double min = INT_MAX;
+        for (int i = 0; i<n ; ++i) {
+            int* arr_new = (int*)malloc((n-1)*sizeof(int));
+            int j = 0; int k = 0;
+            while (j<n) {
+                if (j == i) {
+                    //do nothing
+                }
+                else {
+                    arr_new[k] = j;
+                    ++k; 
+                }
+                ++j;
+            }
+            if (search(mat, arr_new, n-1) < min) {
+                min = search(mat, arr_new, n-1);
+            }
+        }
+    }
+}
---
+++ search.h
@@ -0,0 +1,8 @@
+/**
+    *Fungsi untuk mencari jalur terbaik untuk bisa menghemat waktu
+    *Merupakan algoritma untuk Travelling Salesman Problem
+    
+    *@param mat matriks yang berisi weight dari setiap edge
+    *@param n jumlah vertice dalam masalah yang diberikan
+**/
+double search (double** mat, int* arr, int n);
