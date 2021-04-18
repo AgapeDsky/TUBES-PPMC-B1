072fb1881193c840cbee61acfb65581541f49a5b
Richardmote <78895556+Richardmote@users.noreply.github.com>
2021-04-18 22:47:39 +0700

    Create isValidKoor.h

---
+++ isValidKoor.h
@@ -0,0 +1,10 @@
+#include "math.h"
+
+/**
+    *Fungsi untuk mengecek apakah format latitude dan longitude valid.
+    *Mengeluarkan output 1 bila valid dan 0 bila tidak valid
+    
+    *@param koor token koordinat
+
+**/
+int isValidKoor (char* koor);
